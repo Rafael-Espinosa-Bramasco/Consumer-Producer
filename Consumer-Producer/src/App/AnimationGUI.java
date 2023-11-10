5648415813018850cfb646666c0dab07a97762f8
AndreaGasparMiramontes <142550697+AndreaGasparMiramontes@users.noreply.github.com>
2023-11-09 23:09:09 -0600

    apenas pruebas de animaciones

--- Consumer-Producer/src/App/AnimationGUI.java
+++ Consumer-Producer/src/App/AnimationGUI.java
@@ -4,9 +4,12 @@
  */
 package App;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 /**
  *
- * @author rafael
+ * @author rafael & andrea
  */
 public class AnimationGUI extends javax.swing.JFrame {
 
@@ -15,8 +18,32 @@ public class AnimationGUI extends javax.swing.JFrame {
      */
     public AnimationGUI() {
         initComponents();
+        ElementsInit();
     }
 
+    private void ElementsInit(){
+        Icon productor = new ImageIcon(new ImageIcon(getClass().getResource("/images/productor.png")).getImage()
+        .getScaledInstance(productor_label.getWidth(), productor_label.getHeight(), 0));
+        
+        productor_label.setIcon(productor);
+        
+        WakeConsumidor();
+    }
+    
+    public void SleepConsumidor(){
+        Icon consumidor = new ImageIcon(new ImageIcon(getClass().getResource("/images/consumidor_dormir.png")).getImage()
+        .getScaledInstance(consumidor_label.getWidth(), consumidor_label.getHeight(), 0));
+        
+        consumidor_label.setIcon(consumidor);
+    }
+    
+    public void WakeConsumidor(){
+        Icon consumidor = new ImageIcon(new ImageIcon(getClass().getResource("/images/consumidor.png")).getImage()
+        .getScaledInstance(consumidor_label.getWidth(), consumidor_label.getHeight(), 0));
+        
+        consumidor_label.setIcon(consumidor);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +53,56 @@ public class AnimationGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        productor_label = new javax.swing.JLabel();
+        consumidor_label = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        productor_label.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/productor.png"))); // NOI18N
+
+        consumidor_label.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/consumidor.png"))); // NOI18N
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(consumidor_label, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(productor_label, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(368, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(346, 346, 346))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(149, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(productor_label, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(consumidor_label, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        SleepConsumidor();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +140,8 @@ public class AnimationGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel consumidor_label;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel productor_label;
     // End of variables declaration//GEN-END:variables
 }
