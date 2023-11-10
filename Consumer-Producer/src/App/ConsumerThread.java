254eb693257460aa6412c67a6cc7baecb9fb1034
Rafael Espinosa <rafaskrullantonio@gmail.com>
2023-11-10 00:23:14 -0600

    Main Funtionality Added, please add animations

--- Consumer-Producer/src/App/ConsumerThread.java
+++ Consumer-Producer/src/App/ConsumerThread.java
@@ -4,6 +4,8 @@
  */
 package App;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,18 +16,63 @@ import java.util.logging.Logger;
 public class ConsumerThread extends Thread{
     
     StatusGUI statusREF;
-    int lastIndex;
+    int index;
     
     public ConsumerThread(StatusGUI s){
         this.statusREF = s;
-        this.lastIndex = 0;
+        this.index = 0;
     }
     
     @Override
     public void run(){
+        int ranNum;
+        int actualChanges;
+        
+        Random rand = new Random();
+        
         while(true){
+            try {
+                statusREF.addConsumerMessage("Attempting to work");
+                Thread.sleep(300);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             if(statusREF.isConsumerAvalible()){
                 statusREF.setConsumerStatus(2);
+                ranNum = rand.nextInt(8) + 3;
+                
+                actualChanges = 0;
+                
+                for(int i = 0 ; i < ranNum && statusREF.getContinerSize() > 0 ; i++){
+                    if(index == 35){
+                        index = 0;
+                    }
+                    
+                    statusREF.modCont(index, '*');
+                    index++;
+                    actualChanges++;
+                    statusREF.setContinerSize(statusREF.getContinerSize()-1);
+                    
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                statusREF.addContainerMessage(String.valueOf(actualChanges).concat(" elements consumed!"));
+                statusREF.setStatus(0);
+                
+                try {
+                    statusREF.setConsumerStatus(0);
+                    statusREF.addSystemMessage("Free (No people using the container)");
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                continue;
             }
             
             try {
--- Consumer-Producer/src/App/ProducerThread.java
+++ Consumer-Producer/src/App/ProducerThread.java
@@ -4,6 +4,8 @@
  */
 package App;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,18 +16,67 @@ import java.util.logging.Logger;
 public class ProducerThread extends Thread{
     
     StatusGUI statusREF;
+    int index;
     
     public ProducerThread(StatusGUI s){
         this.statusREF = s;
+        this.index = 0;
     }
     
     @Override
     public void run(){
+        int ranNum;
+        int actualChanges;
+        
+        Random rand = new Random();
+        
         while(true){
+            try {
+                statusREF.addProducerMessage("Attempting to work");
+                Thread.sleep(300);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             if(statusREF.isProducerAvalible()){
+                statusREF.setProducerStatus(2);
+                ranNum = rand.nextInt(8) + 3;
+                
+                actualChanges = 0;
+                
+                for(int i = 0 ; i < ranNum && statusREF.getContinerSize() <= 32 ; i++){
+                    if(index == 35){
+                        index = 0;
+                    }
+                    
+                    statusREF.modCont(index, 'C');
+                    index++;
+                    actualChanges++;
+                    statusREF.setContinerSize(statusREF.getContinerSize()+1);
+                    
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+                statusREF.addContainerMessage(String.valueOf(actualChanges).concat(" elements produced!"));
+                statusREF.setStatus(0);
+                
+                try {
+                    statusREF.setProducerStatus(0);
+                    statusREF.addSystemMessage("Free (No people using the container)");
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                continue;
             }
             
             try {
+                statusREF.setConsumerStatus(1);
                 Thread.sleep(3000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
